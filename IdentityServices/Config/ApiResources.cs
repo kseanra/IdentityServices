417694ed9bbe640bfd91f1717dd8950943901e38
Ken Liang <kenliangnz@gmail.com>
2018-09-11 08:02:23 +1200

    Initial Commit

---
+++ IdentityServices/Config/ApiResources.cs
@@ -0,0 +1,20 @@
+﻿using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServices.Config
+{
+    public static class ApiResources
+    {
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("GmailAPI", "Gmail API"),
+                new ApiResource("MyAPI", "My API")
+            };
+        }
+    }
+}
